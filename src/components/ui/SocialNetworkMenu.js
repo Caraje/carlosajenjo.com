f9e4b007146a92dee0023a0a47d93513e0d057b9
Carlos Ajenjo <Carlosajenjo@gmail.com>
2021-10-11 20:15:54 +0200

    Arreglo redes sociales

--- src/components/ui/SocialNetworkMenu.js
+++ src/components/ui/SocialNetworkMenu.js
@@ -6,9 +6,9 @@ export const SocialNetworkMenu = () => {
             <a href="https://www.linkedin.com/in/carlos-ajenjo-19662076/" target="_blank" rel="noreferrer"><i className="socialnetwork__icon fab fa-linkedin"></i></a>
             <a href="https://twitter.com/caraje_" target="_blank" rel="noreferrer"><i className="socialnetwork__icon fab fa-twitter"></i></a>
             <a href="https://github.com/Caraje" target="_blank" rel="noreferrer"><i className="socialnetwork__icon fab fa-github-square"></i></a>
-            <a href="https://www.instagram.com/carlosajenjo_com/" target="_blank" rel="noreferrer"><i className="socialnetwork__icon fab fa-instagram"></i></a>
-            <a href="https://www.twitch.tv/caraje_" target="_blank" rel="noreferrer"><i className="socialnetwork__icon fab fa-twitch"></i></a>
-            <a href="https://www.youtube.com/channel/UCiY_SNkOx5yAjlujkrJtmCw" target="_blank" rel="noreferrer"><i className="socialnetwork__icon fab fa-youtube"></i></a>
+            <a href="https://www.instagram.com/caraje_dev/" target="_blank" rel="noreferrer"><i className="socialnetwork__icon fab fa-instagram"></i></a>
+            {/* <a href="https://www.twitch.tv/caraje_" target="_blank" rel="noreferrer"><i className="socialnetwork__icon fab fa-twitch"></i></a> */}
+            {/* <a href="https://www.youtube.com/channel/UCiY_SNkOx5yAjlujkrJtmCw" target="_blank" rel="noreferrer"><i className="socialnetwork__icon fab fa-youtube"></i></a> */}
         </div>
     )
 }
